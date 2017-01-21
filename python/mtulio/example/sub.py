b71f1614b11af3a24bd0a8ac358d6474052abed9
Marco Tulio R Braga <marco.braga@chaordicsystems.com>
2017-01-21 20:09:53 -0200

    [python] add examples to work with libs

---
+++ python/mtulio/example/sub.py
@@ -0,0 +1,9 @@
+
+class SubClass():
+    def __init__(self):
+        self.message = 'init - SubFile - mtulio.example.sub Class'
+    def returnMessage(self):
+        return self.message
+
+def subMessageHelloWorld():
+    return 'init - SubFile - mtulio.example.sub Method'
