2c2d689f88185a3e4d7c195fc00a7d2d35918d16
Marco Tulio R Braga <marco.tulio@mtulio.eng.br>
2017-02-15 01:26:26 -0200

    [pymod] add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='common',
+      version='0.1.0',
+      description='Common Python modules',
+      author='Marco Tulio R Braga',
+      author_email='braga@mtulio.eng.br',
+      url='https://github.com/mtulio/common-py',
+      packages=['distutils', 'distutils.command'],
+     )
